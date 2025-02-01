9909869459a3d055adb40f6e4a80af2f48d67fde
Hernandessn <hernandessalesneves7@gmail.com>
2025-02-01 11:50:57 -0300

    feat: add Button component

--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -1,7 +1,7 @@
 export const Logo = () => (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="113"
+      width="143"
       height="20"
       fill="none"
       viewBox="0 0 113 20"
