b7cd250e3b2b7f9d61db48e94801919e4b86273f
Hernandessn <hernandessalesneves7@gmail.com>
2025-02-01 12:16:34 -0300

    feat: add Title component and styles: add in home (Main, Section, Filters)

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,15 +1,27 @@
 import { Button } from "../../components/button";
 import { Logo } from "../../components/logo";
-import { Header } from "./styles";
+import { Title } from "../../components/title";
+import { Filters, Header, Main, Section } from "./styles";
 
 export function Home(){
     return(
-        <Header>
+        <>
+         <Header>
             <Logo />
             <div>
                 <Button>Nova transação</Button>
                 <Button>Nova Categoria</Button>
             </div>
         </Header>
+        <Main>
+        <Section>
+            <Filters>
+                <Title title="Saldo" subtitle="Receitas e despesas no período"/>
+            </Filters>
+        </Section>
+
+        </Main>
+        </>
+       
     )
 }
